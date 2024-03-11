5ca235494a3e35f3eddf8100effd98f5a4805569
Gabriel Pajot <github@gab.lescactus.eu>
2024-03-11 10:49:55 +0100

    chore(ruff): fix format

--- qbee_gpio/gpio/types.py
+++ qbee_gpio/gpio/types.py
@@ -1,6 +1,7 @@
 """
 Minimal GPIO types for static typing and mocking in tests.
 """
+
 from typing import Literal, Union
 
 Bit = Union[bool, Literal[0, 1]]
@@ -11,17 +12,13 @@ class GPIO:
     BCM = 0
 
     @classmethod
-    def setmode(cls, mode: int):
-        ...
+    def setmode(cls, mode: int): ...
 
     @classmethod
-    def setup(cls, pin: int, mode: int):
-        ...
+    def setup(cls, pin: int, mode: int): ...
 
     @classmethod
-    def output(cls, pin: int, value: Bit):
-        ...
+    def output(cls, pin: int, value: Bit): ...
 
     @classmethod
-    def cleanup(cls):
-        ...
+    def cleanup(cls): ...
